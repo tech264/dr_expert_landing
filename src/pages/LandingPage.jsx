d2d4c8451e7c003438208cf84aedde178cd72b21
Javad Jazz <javadjazz899@gmail.com>
2026-06-21 18:28:53 +0530

    done

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -114,6 +114,10 @@ function LandingPage() {
   const [current, setCurrent] = useState(0);
   const [isGeneratingToken, setIsGeneratingToken] = useState(false);
   const timerRef = useRef(null);
+  const [token, setToken] = useState(null)
+  const [retry, setRetry] = useState(false)
+  
+ 
 
   function resetTimer() {
     clearInterval(timerRef.current);
@@ -129,35 +133,54 @@ function LandingPage() {
 
 
 
-async function handleWhatsApp(buttonName) {
-  console.log("Hello world")
-  const urlParams = new URLSearchParams(window.location.search);
-  const utm_campign = urlParams.get('utm_campaign') || '';
-  const utm_adset = urlParams.get('utm_adset') || '';
-  const utm_ad = urlParams.get('utm_ad') || '';
-
-  console.log(urlParams, utm_campign, utm_ad);
-
-  let token = null;
-
-  // https://landing.drexpertedu.com/?utm_source=facebook&utm_campaign=Georgia+%7C+website+%7C+engmt+%7C+16/05/26&utm_adset=Georgia+%7C+website+%7C+Kerala+%7C+engmt+%7C+Ad+Set&utm_ad=Georgia+%7C+Kerala+%7C+Cr+1+%7C+Poster+common&fbclid=PAVERFWASks0tleHRuA2FlbQIxMABzcnRjBmFwcF9pZA8xMjQwMjQ1NzQyODc0MTQAAafa6srdX8hQFHK_SUh1nKbwaEVTUXEmIiP1jhEYXQkFgGRIJapYTT_6MXMk0g_aem_w86XXDx-QW2e9m-2lawyPg
-
-  if (utm_campign || utm_adset || utm_ad) {
-    setIsGeneratingToken(true);
-    try {
-      // const apiUrl = `http://localhost:3000/api/track-utm?utm_campign=${encodeURIComponent(utm_campign)}&utm_adset=${encodeURIComponent(utm_adset)}&utm_ad=${encodeURIComponent(utm_ad)}`;
-      const apiUrl = `https://api.drexpertedu.com/neet-exam/api/track-utm?utm_campaign=${encodeURIComponent(utm_campign)}&utm_adset=${encodeURIComponent(utm_adset)}&utm_ad=${encodeURIComponent(utm_ad)}`;
-      const response = await fetch(apiUrl);
-      const data = await response.json();
-      if (data.success) token = data.token;
-    } catch (error) {
-      console.error("Error tracking UTM:", error);
+  function handleWhatsApp(buttonName) {
+    if (token) {
+        window.openWhatsApp(buttonName, token);
+    } else {
+      setIsGeneratingToken(true)
+      setRetry(false)
     }
-    setIsGeneratingToken(false);
   }
 
-  window.openWhatsApp(buttonName, token);
-}
+ 
+
+  useEffect(() => {
+    if (!token) {
+
+      async function handleWhatsAppApi() {
+        console.log("Hello world")
+        const urlParams = new URLSearchParams(window.location.search);
+        const utm_campign = urlParams.get('utm_campaign') || '';
+        const utm_adset = urlParams.get('utm_adset') || '';
+        const utm_ad = urlParams.get('utm_ad') || '';
+    
+        console.log(urlParams, utm_campign, utm_ad);
+    
+        let token = null;
+    
+        // https://landing.drexpertedu.com/?utm_source=facebook&utm_campaign=Georgia+%7C+website+%7C+engmt+%7C+16/05/26&utm_adset=Georgia+%7C+website+%7C+Kerala+%7C+engmt+%7C+Ad+Set&utm_ad=Georgia+%7C+Kerala+%7C+Cr+1+%7C+Poster+common&fbclid=PAVERFWASks0tleHRuA2FlbQIxMABzcnRjBmFwcF9pZA8xMjQwMjQ1NzQyODc0MTQAAafa6srdX8hQFHK_SUh1nKbwaEVTUXEmIiP1jhEYXQkFgGRIJapYTT_6MXMk0g_aem_w86XXDx-QW2e9m-2lawyPg
+    
+        if (utm_campign || utm_adset || utm_ad) {
+          // setIsGeneratingToken(true);
+          try {
+            // const apiUrl = `http://localhost:3000/api/track-utm?utm_campign=${encodeURIComponent(utm_campign)}&utm_adset=${encodeURIComponent(utm_adset)}&utm_ad=${encodeURIComponent(utm_ad)}`;
+            const apiUrl = `https://api.drexpertedu.com/neet-exam/api/track-utm?utm_campaign=${encodeURIComponent(utm_campign)}&utm_adset=${encodeURIComponent(utm_adset)}&utm_ad=${encodeURIComponent(utm_ad)}`;
+            const response = await fetch(apiUrl);
+            const data = await response.json();
+            if (data.success) token = data.token;
+          } catch (error) {
+            console.error("Error tracking UTM:", error);
+          }
+          // setIsGeneratingToken(false);
+        }
+    
+        setToken(token)
+        setIsGeneratingToken(false)
+      }
+
+      handleWhatsAppApi()
+    }
+  },[retry])
 
   return (
     <div style={{ fontFamily: "'Inter', sans-serif", background: '#f5f5f5', color: '#0a0a0a', overflowX: 'hidden', minHeight: '100vh' }}>
